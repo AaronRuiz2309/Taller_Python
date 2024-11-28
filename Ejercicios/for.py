96e0784102cb318c864efd2c353c37b8c3c67495
AaronRuiz2309 <168696202+AaronRuiz2309@users.noreply.github.com>
2024-11-28 13:35:08 -0600

    Update for.py

--- Ejercicios/for.py
+++ Ejercicios/for.py
@@ -1,13 +1,10 @@
-# Ejercicio 1
 for _ in range(5):
     print("Python")
 
-# Ejercicio 2
 limite = int(input("Ingresa un valor: "))
 for i in range(1, limite + 1):
     print(i)
 
-# Ejercicio 3
 limite = int(input("Ingresa un valor: "))
 for i in range(0, limite + 1, 2):
     print(i)
